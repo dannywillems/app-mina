4ad0bd31eafe42ddf5bc844c2570190efe2245fa
Sarah GLINER <sarah.gliner@ledger.fr>
2023-01-11 18:47:55 +0100

    get_address: update for stax

--- src/get_address.h
+++ src/get_address.h
@@ -2,5 +2,7 @@
 
 #include "globals.h"
 
+void ui_get_address(uint8_t *dataBuffer);
+
 void handle_get_address(uint8_t p1, uint8_t p2, uint8_t *dataBuffer,
                         uint8_t dataLength, volatile unsigned int *flags);
